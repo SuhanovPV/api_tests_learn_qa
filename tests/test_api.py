deaa835137ad9f7280252928fe7ffea484c27288
Sukhanov Pavel <ssuxxarr@gmail.com>
2025-01-12 18:47:53 +0300

    Added allure annotation

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,11 @@ from api_tests_learn_qa.utils import get_json_schema, custom_requests
 from api_tests_learn_qa.data.user import User
 from api_tests_learn_qa.data.data_generator import email_generator
 
-
+@allure.epic('api')
+@allure.story('Test api methods for work with user ')
+@allure.tag('api', 'user', 'create')
+@allure.title('Create user with unique email')
+@allure.severity(allure.severity_level.CRITICAL)
 def test_create_user():
     user = User('ssuxxarr', 'Pavel', 'Svirin', email_generator(), '1234')
     with allure.step(f'Create new user {user}'):
@@ -14,7 +18,11 @@ def test_create_user():
         assert response.status_code == 200
         validate(response.json(), get_json_schema.get_user_register_schema())
 
-
+@allure.epic('api')
+@allure.story('Test api methods for work with user ')
+@allure.tag('api', 'user', 'create')
+@allure.title('Create user with already used email')
+@allure.severity(allure.severity_level.NORMAL)
 def test_create_user_with_already_used_email(create_user):
     second_user = User('login_1', 'Johnybek', 'Aliev', create_user.email, '8534')
     with allure.step(f'Create new user {create_user}'):
@@ -23,7 +31,11 @@ def test_create_user_with_already_used_email(create_user):
         assert response_second_user.status_code == 400
         assert response_second_user.text == f"Users with email '{create_user.email}' already exists"
 
-
+@allure.epic('api')
+@allure.story('Test api methods for work with user ')
+@allure.tag('api', 'user', 'auth')
+@allure.title('Authorization')
+@allure.severity(allure.severity_level.BLOCKER)
 def test_user_authorization(create_user):
     with allure.step(f'Login as {create_user}'):
         response_login = custom_requests.post('/user/login', data=create_user.get_credentials())
@@ -34,7 +46,11 @@ def test_user_authorization(create_user):
         assert 'x-csrf-token' in response_login.headers
         assert response_login.json()['user_id'] == int(create_user.id)
 
-
+@allure.epic('api')
+@allure.story('Test api methods for work with user ')
+@allure.tag('api', 'user', 'get_info')
+@allure.title('Get user info with authorization by same user')
+@allure.severity(allure.severity_level.CRITICAL)
 def test_get_user_info_with_authorization_by_same_user(user_authorization):
     user = user_authorization['user']
     auth_sid = user_authorization['auth_sid']
@@ -49,7 +65,11 @@ def test_get_user_info_with_authorization_by_same_user(user_authorization):
         for key in user.get_info():
             assert response.json()[key] == user.get_info()[key]
 
-
+@allure.epic('api')
+@allure.story('Test api methods for work with user ')
+@allure.tag('api', 'user', 'get_info')
+@allure.title('Get user info with authorization by another user')
+@allure.severity(allure.severity_level.MINOR)
 def test_get_user_info_with_authorization_by_another_user(create_user, another_user_authorization):
     auth_sid = another_user_authorization['auth_sid']
     token = another_user_authorization['x-csrf-token']
@@ -65,7 +85,11 @@ def test_get_user_info_with_authorization_by_another_user(create_user, another_u
         validate(user_info_response.json(), get_json_schema.get_not_auth_user_info_schema())
         assert user_info_response.json()['username'] == user.username
 
-
+@allure.epic('api')
+@allure.story('Test api methods for work with user ')
+@allure.tag('api', 'user', 'modify')
+@allure.title('Modify user data with authorization by same user')
+@allure.severity(allure.severity_level.CRITICAL)
 def test_modify_user_data_with_authorization_by_same_user(user_authorization):
     user = user_authorization['user']
     auth_sid = user_authorization['auth_sid']
@@ -92,7 +116,11 @@ def test_modify_user_data_with_authorization_by_same_user(user_authorization):
         assert user_info_response.json()['email'] == user.get_info()['email']
         assert user_info_response.json()['username'] == user.get_info()['username']
 
-
+@allure.epic('api')
+@allure.story('Test api methods for work with user ')
+@allure.tag('api', 'user', 'modify')
+@allure.title('Modify user data with authorization by another user')
+@allure.severity(allure.severity_level.MINOR)
 def test_modify_user_data_with_authorization_by_another_user(user_authorization, another_user_authorization):
     another_user_auth_sid = another_user_authorization['auth_sid']
     another_user_token = another_user_authorization['x-csrf-token']
@@ -120,7 +148,11 @@ def test_modify_user_data_with_authorization_by_another_user(user_authorization,
         for key in user.get_info():
             assert user_info_response.json()[key] == user.get_info()[key]
 
-
+@allure.epic('api')
+@allure.story('Test api methods for work with user ')
+@allure.tag('api', 'user', 'delete')
+@allure.title('Delete user with authorization by same user')
+@allure.severity(allure.severity_level.NORMAL)
 def test_delete_user_with_authorization_by_same_user(user_authorization):
     user = user_authorization['user']
     auth_sid = user_authorization['auth_sid']
@@ -136,7 +168,11 @@ def test_delete_user_with_authorization_by_same_user(user_authorization):
         get_user_response = custom_requests.get(f'/user/{user.id}')
         assert get_user_response.status_code == 404
 
-
+@allure.epic('api')
+@allure.story('Test api methods for work with user ')
+@allure.tag('api', 'user', 'delete')
+@allure.title('Delete user with authorization by another user')
+@allure.severity(allure.severity_level.NORMAL)
 def test_delete_user_with_authorization_by_another_user(create_user, another_user_authorization):
     another_user_auth_sid = another_user_authorization['auth_sid']
     another_user_token = another_user_authorization['x-csrf-token']
